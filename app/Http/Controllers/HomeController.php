a454712fd9bff114e6a867c8d213fe1c059e2a3d
Kenta Suzuki <kenta.suzuki1014@gmail.com>
2022-04-17 21:22:22 +0900

    feat: Memo update process

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -41,4 +41,26 @@ class HomeController extends Controller
         Memo::insert(['content' => $posts['content'], 'user_id' => \Auth::id()]);
         return redirect( route('home') );
     }
+
+    public function edit($id)
+    {
+        // ここでメモを取得
+        $memos = Memo::select('memos.*')
+            ->where('user_id', '=', \Auth::id())
+            ->whereNull('deleted_at')
+            ->orderBy('updated_at', 'DESC')// ASC=小さい順
+            ->get();
+
+        $edit_memo = Memo::find($id);
+        return view('edit', compact('memos', 'edit_memo'));
+    }
+
+    public function update(Request $request)
+    {
+        $posts = $request->all();
+        // dump dieの略 - メソッドの引数を撮った値を展開して止める
+
+        Memo::where('id', $posts['memo_id'])->update(['content' => $posts['content']]);
+        return redirect( route('home') );
+    }
 }
